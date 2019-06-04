8cbe94c0e601083cf991674376a0d96dafaddfc7
Palak Mittal <44226699+palak2001@users.noreply.github.com>
2019-06-04 14:50:11 +0530

    Update timetable.java

--- timetable.java
+++ timetable.java
@@ -2,8 +2,16 @@ import java.util.Scanner;
 
 public class timetable
 {
-    String a[][];
-    a= new int[7][7];
+    public class elements
+    {
+        String code;
+        String name;
+        String ins;
+        int pri;
+        int slots;
+        int dur;
+        int time;
+    }
     public static void main(String args[])
     {
         Scanner t= new Scanner(System.in);
@@ -29,15 +37,91 @@ public class timetable
             Scanner couses= new Scanner(System.in);
             for(i=0;i<courses;i++)
             {
-                Scanner code[i]= new Scanner(System.in);
-                Scanner name[i]= new Scanner(System.in);
-                Scanner ins[i]= new Scanner(System.in);
-                Scanner pri[i]= new Scanner(System.in);
-                Scanner slots[i]= new Scanner(System.in);
-                Scanner dur[i]= new Scanner(System.in);
-                Scanner day[i]= new Scanner(System.in);
-                Scanner time[i]= new Scanner(System.in);
-                if(dur==2)
+                elements a[i]= a[i].elements();
+                Scanner a[i].code= new Scanner(System.in);
+                Scanner a[i].name= new Scanner(System.in);
+                Scanner a[i].ins= new Scanner(System.in);
+                Scanner a[i].pri= new Scanner(System.in);
+                Scanner a[i].slots= new Scanner(System.in);
+                Scanner a[i].dur= new Scanner(System.in);
+                Scanner a[i].day= new Scanner(System.in);
+                Scanner a[i].time= new Scanner(System.in);
+                
+            }
+            //sort a[i] acc to pri
+            for(i=0;i<courses,i++)
+            {
+                if(a[i].dur==2)
+                {
+                    for(j=daystart;j<dayend;j++)
+                    {
+                        for(k=daystart;k<=dayend;k++)
+                        {
+                            if(d[j][k]==NULL)&&d[j+1][k])
+                            {
+                                d[j][k]=a[i];
+                                flag=1;
+                                break;
+                            }
+                        }
+                    }
+                    if(flag)
+                    break;
+                    else
+                    {
+                        for(j=daystart;j<dayend;j++)
+                        {
+                            for(k=1;k<=7;k++)
+                            {
+                                if(d[j][k]==NULL)&&d[j+1][k])
+                                {
+                                    d[j][k]=a[i];
+                                    flag=1;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    if(flag)
+                    break;
+                    else
+                    if((d[0][]==NULL)&&(d[3][]==NULL))
+                    {
+                        
+
+
+                    }
+                    else if ((d[1][]==NULL)||(d[2][]==NULL))
+                    {
+
+                    }
+                    else if((d[4][]==NULL)&&(d[5][]==NULL))
+                    {
+
+                    }
+                    else if((d[5][]==NULL)&&(d[6][]==NULL))
+                    {
+
+                    }
+                    else
+                    {
+                        //Saturday
+                    }
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+                /*if(dur==2)
                 {
                     if((a[0][]==NULL)&&(a[3][]==NULL))
                     {
@@ -63,8 +147,4 @@ public class timetable
                 else
                 {
 
-                }
-            }
-        }
-    }
-}
+                }*/
